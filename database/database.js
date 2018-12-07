d280f3b1c23bb2562c490279591ad3057df7f223
dibidave <dibidave@gmail.com>
2018-12-07 13:54:27 -0800

    Database upgrades supported for both sqit and user versions, resolves #3

--- database/database.js
+++ database/database.js
@@ -14,11 +14,16 @@ module.exports = {
 
       db = client_connection.db(config.database_name);
 
-      return update_database();
+      return upgrade_database(SQIT_VERSION_UPGRADE_MAP, SQIT_DATABASE_VERSION,
+        false);
 
     });
 
     return promise;
+  },
+
+  upgrade: function(database_upgrade_map, latest_version) {
+    return upgrade_database(database_upgrade_map, latest_version, true);
   },
 
   insert: function(collection_name, object) {
@@ -95,8 +100,15 @@ module.exports = {
     var promise = collection.deleteMany(filter);
 
     return promise;
+  },
+
+  update_many: function(collection_name, filter, update) {
+    
+    var collection = db.collection(collection_name);
+    
+    return collection.updateMany(filter, update);
   }
-}
+};
 
 var MongoClient = require("mongodb").MongoClient;
 var ObjectID = require("mongodb").ObjectID;
@@ -104,31 +116,27 @@ var client_connection = null;
 var db = null;
 var logger = require("../logging/Logger").get_logger("database");
 var config = require("../config/config");
-const DATABASE_VERSION = 1;
+const SQIT_DATABASE_VERSION = 2;
 
-var update_database = function() {
+var upgrade_database = function(version_upgrade_map, latest_version, 
+  is_user_database) {
 
-  logger.info("Verifying database is on latest version, v" + DATABASE_VERSION);
+  var database_type = get_metadata_collection_name(is_user_database);
 
-  var promise = get_database_version()
+  logger.info("Verifying " + database_type + " db is on latest version, v"
+    + latest_version);
+
+  var promise = get_database_version(database_type)
   .then(function(current_database_version) {
-    if(current_database_version < DATABASE_VERSION) {
+    if(current_database_version < latest_version) {
 
       var promise = null;
 
-      switch(current_database_version) {
-        case 0:
-          promise = create_database();
-          break;
-        default:
-          logger.error("No database upgrade specified for v" +
-            current_database_version);
-          promise = Promise.reject();
-      }
-
-      promise = promise
-      .then(set_database_version.bind(null, current_database_version + 1)
-      ).then(update_database);
+      promise = version_upgrade_map[current_database_version]()
+      .then(set_database_version.bind(null, current_database_version + 1,
+        database_type)
+      ).then(upgrade_database.bind(null, version_upgrade_map, latest_version,
+        is_user_database));
     }
     else {
       return Promise.resolve();
@@ -138,24 +146,37 @@ var update_database = function() {
   return promise;
 };
 
-var create_database = function() {
-  return Promise.resolve();
+var get_metadata_collection_name = function(is_user_database) {
+
+  if(is_user_database === null || is_user_database === undefined) {
+    is_user_database = true;
+  }
+
+  if(is_user_database) {
+    collection_name = config.database_name;
+  }
+  else {
+    collection_name = "sqit";
+  }
+
+  return collection_name;
 };
 
-var set_database_version = function(version) {
-  
-  logger.debug("Setting database version to " + version);
+var set_database_version = function(version, database_name) {
 
-  var metadata_collection = db.collection("Metadata");
+  logger.debug("Setting " + database_name + " database version to "
+    + version);
+
+  var metadata_collection = db.collection("_metadata");
 
   var promise = metadata_collection.updateOne(
     {
-      collection_name: "scrap"
+      collection_name: database_name
     },
     {
       $set: {
         "version": version,
-        "collection_name": "scrap"
+        "collection_name": database_name
       }
     },
     {
@@ -164,25 +185,25 @@ var set_database_version = function(version) {
   );
 
   return promise;
-}
+};
 
-var get_database_version = function() {
+var get_database_version = function(database_name) {
 
-  logger.debug("Getting database version");
+  logger.debug("Getting " + database_name + " database version");
 
-  var metadata_collection = db.collection("Metadata");
+  var metadata_collection = db.collection("_metadata");
 
   var promise = metadata_collection.findOne(
   {
-    collection_name: "scrap"
+    collection_name: database_name
   }).then(function(metadata) {
 
     if(metadata === null) {
-      logger.debug("Database metadata not found, assuming version 0");
+      logger.debug("Metadata not found, assuming version 0");
       return 0;
     }
     else if(!metadata.hasOwnProperty("version")) {
-      logger.debug("Database metadata not found, assuming version 0");
+      logger.debug("Metadata not found, assuming version 0");
       return 0;
     }
     else {
@@ -242,4 +263,27 @@ var convert_ids_to_object_ids = function(object, is_candidate) {
   }
 
   return object;
+};
+
+var create_database = function() {
+  return set_database_version(0, get_metadata_collection_name(false));
+};
+
+var upgrade_database_v_1 = function() {
+
+  var old_metadata_collection_name = "Metadata";
+
+  var promise = db.dropCollection(old_metadata_collection_name)
+  .then(function() {
+    return Promise.resolve();
+  }).catch(function(error) {
+    return Promise.resolve();
+  });
+
+  return promise;
+};
+
+const SQIT_VERSION_UPGRADE_MAP = {
+  0: create_database,
+  1: upgrade_database_v_1
 };
